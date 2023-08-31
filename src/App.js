edf1bdd8b447e17bb3755173e65865efbe9b46d9
jose <torrj755@gmail.com>
2023-08-31 14:46:04 -0600

    m

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import React, { useState, Fragment } from 'react';
+import Pagination from './Pagination';
+import useDataApi from './useDataApi';
 import './App.css';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 10;
+  const [{ data, isLoading, isError }, doFetch] = useDataApi(
+    "https://hn.algolia.com/api/v1/search?query=MIT",
+    {
+      hits: [],
+    }
+  );
+
+  const handlePageChange = (e) => {
+    setCurrentPage(Number(e.target.textContent));
+  };
+
+  let page = data.hits;
+  if (page.length >= 1) {
+    page = paginate(page, currentPage, pageSize);
+    console.log(`currentPage: ${currentPage}`);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+        <ul className="list-group">
+          {page.map((item) => (
+            <li key={item.objectID} className="list-group-item">
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+      <Pagination
+        items={data.hits}
+        pageSize={pageSize}
+        onPageChange={handlePageChange}
+      ></Pagination>
+    </Fragment>
   );
 }
 
---
+++ src/Pagination.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import './Pagination.css';
+
+function Pagination({ items, pageSize, onPageChange }) {
+  if (items.length <= 1) return null;
+
+  let num = Math.ceil(items.length / pageSize);
+  let pages = range(1, num + 1);
+  const list = pages.map((page) => {
+    return (
+      <Button key={page} onClick={onPageChange} className="page-item">
+        {page}
+      </Button>
+    );
+  });
+
+  return (
+    <nav>
+      <ul className="pagination">{list}</ul>
+    </nav>
+  );
+}
+
+const range = (start, end) => {
+  return Array(end - start + 1)
+    .fill(0)
+    .map((item, i) => start + i);
+};
+
+export default Pagination;
